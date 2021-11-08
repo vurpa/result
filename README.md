597bc8f61e7e0b7c7e8b065f563d128d0ae0945a
john <js@thesc.se>
2021-11-08 13:12:44 +0100

    add tests

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Result
 Functional Result class with generics via annotations.
 
+## Tests
+Run
+
+```
+vendor/bin/phpunit tests
+```
+
 ## Links
 https://enterprisecraftsmanship.com/posts/functional-c-handling-failures-input-errors/
 https://khalilstemmler.com/articles/enterprise-typescript-nodejs/handling-errors-result-class/
--- tests/ResultTest.php
+++ tests/ResultTest.php
@@ -7,6 +7,22 @@ use Vurpa\Result\Result;
 
 final class ResultTest extends TestCase
 {
+    public function testSuccessWithNullValueResult()
+    {
+        $success = Result::ok();
+
+        $this->assertTrue($success->isSuccess());
+        $this->assertNull($success->getValue());
+    }
+
+    public function testFailureResult()
+    {
+        $failure = Result::fail('Invalid operation');
+
+        $this->assertTrue($failure->isFailure());
+        $this->assertSame('Invalid operation', $failure->getError());
+    }
+
     public function testCombineSuccessAndFailureResults()
     {
         $success = Result::ok();
